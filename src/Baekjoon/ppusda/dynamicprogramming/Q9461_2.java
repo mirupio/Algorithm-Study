a06f51199967675d9ce440c0ec5a678b185366d5
mirupio <162397816+mirupio@users.noreply.github.com>
2025-10-20 17:24:20 +0900

    백준9461 - 파도반 수열 -> 인덱스 1부터 시작하도록 리팩토링

---
+++ src/Baekjoon/ppusda/dynamicprogramming/Q9461_2.java
@@ -0,0 +1,47 @@
+package Baekjoon.ppusda.dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q9461_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+
+            long[] dp = new long[N+1];
+            long[] init = {0,1,1,1,2,2};
+
+            /*
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
+            dp[3] = 1;
+            dp[4] = 2;
+            dp[5] = 2;
+             */
+
+            if(N <= 5){
+                System.out.println(init[N]);
+            }
+            else{
+                System.arraycopy(init, 0, dp, 0, init.length);
+                for(int i = 6; i <= N; i++) {
+                    dp[i] = dp[i-5] + dp[i-1];
+                }
+                /*
+                for(int i=0;i<N;i++){
+                    System.out.print(dp[i]+" ");
+                }
+                System.out.println();
+                 */
+
+                System.out.println(dp[N]);
+            }
+        }
+    }
+}
