d99d8368fbfa96a33a921ab31dedd6d95d427182
mirupio <162397816+mirupio@users.noreply.github.com>
2025-10-23 00:01:23 +0900

    백준1003 - 피보나치 함수

---
+++ src/Baekjoon/solvedac/dynamicprogramming/Q1003.java
@@ -0,0 +1,39 @@
+package Baekjoon.solvedac.dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0){
+            int N = Integer.parseInt(br.readLine());
+
+            int[] dp_0 = new int[N + 1];
+            int[] dp_1 = new int[N + 1];
+            dp_0[0] = 1;
+            dp_1[0] = 0;
+            if (N >= 1){
+                dp_0[1] = 0;
+                dp_1[1] = 1;
+            }
+
+            for (int i = 2; i <= N; i++) {
+                dp_0[i] = dp_0[i - 2] + dp_0[i - 1];
+                dp_1[i] = dp_1[i - 2] + dp_1[i - 1];
+            }
+
+            /*
+            for(int i=0;i<N+1;i++){
+                System.out.print(dp_0[i] + " ");
+            }
+            System.out.println();
+             */
+
+            System.out.println(dp_0[N] + " " + dp_1[N]);
+        }
+    }
+}
