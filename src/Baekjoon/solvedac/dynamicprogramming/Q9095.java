eaa584b6c1a85acfa706198ab59ed10e61068e58
mirupio <162397816+mirupio@users.noreply.github.com>
2025-11-04 14:23:47 +0900

    백준9095번 - 1,2,3 더하기

---
+++ src/Baekjoon/solvedac/dynamicprogramming/Q9095.java
@@ -0,0 +1,41 @@
+package Baekjoon.solvedac.dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q9095 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] dp = new int[n+1];
+
+            dp[1] = 1;
+
+            if (n >= 2){
+                dp[2] = 2;
+            }
+
+            if (n >= 3){
+                dp[3] = 4;
+            }
+
+            for(int i=4;i<=n;i++){
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            }
+
+            /*for(int i=0 ; i<=n; i++){
+                System.out.print(dp[i] + " ");
+            }
+            System.out.println();*/
+
+            System.out.println(dp[n]);
+        }
+
+    }
+}
