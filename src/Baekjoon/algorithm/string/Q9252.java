f8ad95da932556a464ca2bd1f1be1d400d441aa9
mirupio <162397816+mirupio@users.noreply.github.com>
2025-12-26 17:34:58 +0900

    백준9252 - LCS 2

---
+++ src/Baekjoon/algorithm/string/Q9252.java
@@ -0,0 +1,51 @@
+package Baekjoon.algorithm.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q9252 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String first = br.readLine();
+        String second = br.readLine();
+
+        int[][] dp = new int[first.length()+1][second.length()+1];
+
+        for (int i = 1; i <= first.length(); i++) {
+            for (int j = 1; j <= second.length(); j++) {
+                if (first.charAt(i-1) == second.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); // first or second의 현재 문자 버리기
+                }
+            }
+        }
+
+        System.out.println(dp[first.length()][second.length()]);
+
+        int i = first.length();
+        int j = second.length();
+        String LCS = "";
+        while (i > 0 && j > 0) {
+            if(first.charAt(i-1) == second.charAt(j-1)) {
+                LCS += first.charAt(i - 1);
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i-1][j] > dp[i][j-1]) {
+                    i--;
+                }
+                else{
+                    j--;
+                }
+            }
+        }
+        for (int c = LCS.length() - 1; c >= 0; c--) {
+            System.out.print(LCS.charAt(c));
+        }
+    }
+}
