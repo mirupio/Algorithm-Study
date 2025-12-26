2b5b1b5ecdd6c5180453991a633aff79da6a619a
mirupio <162397816+mirupio@users.noreply.github.com>
2025-12-26 17:54:16 +0900

    주석 추가

--- src/Baekjoon/algorithm/string/Q9251.java
+++ src/Baekjoon/algorithm/string/Q9251.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// LCS
 public class Q9251 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- src/Baekjoon/algorithm/string/Q9252.java
+++ src/Baekjoon/algorithm/string/Q9252.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// LCS 2
 public class Q9252 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
