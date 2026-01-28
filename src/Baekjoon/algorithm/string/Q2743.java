edf51adc8ab39e3a23b7365b3eab33b33d7760f0
mirupio <162397816+mirupio@users.noreply.github.com>
2026-01-28 23:57:44 +0900

    백준2743 - 단어 길이 재기

---
+++ src/Baekjoon/algorithm/string/Q2743.java
@@ -0,0 +1,12 @@
+package Baekjoon.algorithm.string;
+
+import java.io.*;
+
+public class Q2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        System.out.print(s.length());
+    }
+}
