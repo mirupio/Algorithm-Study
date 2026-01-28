f6fc10723d36d64581d047009a0e9162e7f67731
mirupio <162397816+mirupio@users.noreply.github.com>
2026-01-29 00:01:59 +0900

    백준14425 - 문자열 집합

---
+++ src/Baekjoon/algorithm/string/Q14425.java
@@ -0,0 +1,31 @@
+package Baekjoon.algorithm.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Q14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<String> set = new HashSet<>();
+        while (N-- > 0) {
+            set.add(br.readLine());
+        }
+
+        int num = 0;
+        while(M-- > 0) {
+            if (set.contains(br.readLine())) {
+                num++;
+            }
+        }
+
+        System.out.print(num);
+    }
+}
