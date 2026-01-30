8de9fc9fda6101ff21aaf891c6e55e33ec488761
mirupio <162397816+mirupio@users.noreply.github.com>
2026-01-30 22:20:26 +0900

    프로그래머스42576 - 완주하지 못한 선수

---
+++ src/programmers/hash/Q42576.java
@@ -0,0 +1,18 @@
+package programmers.hash;
+
+import java.util.*;
+
+class Q42576 {
+    public String solution(String[] participant, String[] completion) {
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(int i=0;i<completion.length;i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[participant.length-1];
+    }
+}
