72b2e83861552a3fbb7a29e16a0cf5b6eacf787c
mirupio <162397816+mirupio@users.noreply.github.com>
2026-02-09 14:00:07 +0900

    백준11650 - 좌표 정렬하기

---
+++ src/Baekjoon/algorithm/map_set/Q11650.java
@@ -0,0 +1,36 @@
+package Baekjoon.algorithm.map_set;
+
+import java.io.*;
+import java.util.*;
+
+public class Q11650 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[i][0] = x;
+            arr[i][1] = y;
+        }
+
+        Arrays.sort(arr, (a,b) -> {
+            // x좌표 오름차순
+            if(a[0] != b[0]) return Integer.compare(a[0], b[0]);
+
+            // y좌표 오름차순
+            return Integer.compare(a[1],b[1]);
+        });
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<2;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
