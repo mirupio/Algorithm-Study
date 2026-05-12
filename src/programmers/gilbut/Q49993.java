1d858780db98a3bbe5e03c0410ba89cf6168518b
Jeongha Park <162397816+mirupio@users.noreply.github.com>
2026-05-12 16:27:32 +0900

    프로그래머스49993 - 스킬트리

---
+++ src/programmers/gilbut/Q49993.java
@@ -0,0 +1,38 @@
+package programmers.gilbut;
+
+class Q49993 {
+    public int solution(String skill, String[] skill_trees) {
+
+        int count = 0;
+        for(int i=0;i<skill_trees.length;i++){
+            String s = skill_trees[i]; // AECB
+
+            int num=0;
+            char cur_skill = skill.charAt(num);
+            boolean pass = true;
+            for(int j=0;j<s.length();j++){
+                if(skill.contains(s.substring(j,j+1))){
+                    if(s.charAt(j)==cur_skill){
+                        if(num+1<skill.length()){
+                            num++;
+                            cur_skill = skill.charAt(num);
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                    else{
+                        pass = false;
+                        break;
+                    }
+                }
+            }
+            if(pass){
+                count++;
+            }
+        }
+
+
+        return count;
+    }
+}
