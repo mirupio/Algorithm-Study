b9e25cb0601bea516e05aa13560f82b3c464b938
Jeongha Park <162397816+mirupio@users.noreply.github.com>
2026-07-20 13:24:35 +0900

    프로그래머스131130 - 혼자 놀기의 달인 -> 리팩토링 - O(N^2)에서 O(N)으로

--- src/programmers/algorithm/dfsbfs/Q131130.java
+++ src/programmers/algorithm/dfsbfs/Q131130.java
@@ -3,44 +3,40 @@ package programmers.algorithm.dfsbfs;
 class Q131130 {
     static int[] Cards;
     static boolean[] visited;
-    static int first,second;
+
     public int solution(int[] cards) {
         int answer = 0;
         Cards = cards;
 
-        int Max = 0;
+        int firstMax = 0;
+        int secondMax = 0;
+        visited = new boolean[cards.length];
         for(int i=0;i<Cards.length;i++){
-            visited = new boolean[cards.length];
-            first = 0;
-            dfs(i,0,1);
-            //System.out.println("first="+first);
-            second = 0;
-            for(int j=0;j<Cards.length;j++){
-                if(!visited[j]){
-                    dfs(j,0,2);
+            if(!visited[i]){
+                int size = dfs(i);
+
+                if(size>firstMax){
+                    secondMax = firstMax;
+                    firstMax = size;
+                }
+                else if(size>secondMax){
+                    secondMax = size;
                 }
             }
-            //System.out.println("second="+second);
-            Max = Math.max(Max,first*second);
         }
 
-        return Max;
+        return firstMax*secondMax;
     }
 
-    static void dfs(int now,int count,int i){
-        //System.out.println("now="+now+" count="+count);
+    static int dfs(int now){
         visited[now] = true;
-        count++;
-        if(i==1){
-            first=count;
-        }
-        else{
-            second=Math.max(second,count);
-        }
 
         int next = Cards[now]-1;
+
         if(!visited[next]){
-            dfs(next,count,i);
+            return 1+dfs(next);
         }
+
+        return 1;
     }
 }
