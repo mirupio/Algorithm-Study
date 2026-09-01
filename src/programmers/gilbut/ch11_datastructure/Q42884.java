106436f36d139984442c794a8a0b156d14c977fd
Jeongha Park <162397816+mirupio@users.noreply.github.com>
2026-09-01 15:03:28 +0900

    프로그래머스42884 - 단속카메라

---
+++ src/programmers/gilbut/ch11_datastructure/Q42884.java
@@ -0,0 +1,35 @@
+package programmers.gilbut.ch11_datastructure;
+
+import java.util.*;
+
+class Q42884 {
+    public int solution(int[][] routes) {
+        int answer = 0;
+
+        Arrays.sort(routes,(a,b)->a[0]-b[0]);
+
+        int count = 1;
+        int s1 = routes[0][0]; // -20
+        int e1 = routes[0][1]; // -15
+        for(int i=1;i<routes.length;i++){
+            int[] route = routes[i];
+            int s2 = route[0];
+            int e2 = route[1];
+
+            // 겹치지 않으면 카메라 개수++
+            if(e1 < s2){
+                count++;
+                s1 = s2;
+                e1 = e2;
+            }
+
+            else{
+                s1 = Math.max(s1,s2);
+                e1 = Math.min(e1,e2);
+            }
+
+        }
+
+        return count;
+    }
+}
