3a66c1bc762282a3c1239288e234240f213e3685
Jeongha Park <162397816+mirupio@users.noreply.github.com>
2026-09-24 22:20:21 +0900

    프로그래머스150369 - 택배 배달과 수거하기

---
+++ src/programmers/algorithm/implementation/Q150369.java
@@ -0,0 +1,23 @@
+package programmers.algorithm.implementation;
+class Q150369 {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+
+        long answer = 0;
+        int delivery = 0;
+        int pickup = 0;
+
+        for(int i=n-1;i>=0;i--){
+            delivery += deliveries[i];
+            pickup += pickups[i];
+
+            while(delivery>0 || pickup>0){
+                delivery -= cap;
+                pickup -= cap;
+
+                answer += (long) (i+1)*2;
+            }
+        }
+
+        return answer;
+    }
+}
